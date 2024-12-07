4b787d552982132c08d4b532869174a48ec4e33f
alousseynou Diallo <dialloalousseynou1997@gmail.com>
2024-12-07 01:14:47 +0100

    voit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {BrowserRouter} from "react-router"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<>
-    <BrowserRouter>
+    <BrowserRouter basename='/accueil'>
       <App />
     </BrowserRouter>
    
